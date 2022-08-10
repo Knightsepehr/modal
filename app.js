dc0e0a0fe1cb831aee36b4d13cc247cb2e6a4c71
meowmeowkhan <sepehrippirani@gmail.com>
2022-08-10 14:59:20 +0430

    added files

---
+++ app.js
@@ -0,0 +1,18 @@
+const launchBtn = document.querySelector("#modal-launch");
+const modal = document.querySelector(".modal");
+const modalCloseBtn = document.querySelector("#close-modal-btn");
+const modalText = document.querySelector("#modal-text");
+const modalMessage = document.querySelector("#modal-message");
+
+const openModal = (e)=>{
+    e.preventDefault();
+    modal.classList.add("modal-show");
+    modalText.innerText = modalMessage.value;
+}
+const closeModal = (e)=>{
+    e.preventDefault();
+    modal.classList.remove("modal-show");
+}
+launchBtn.addEventListener('click',openModal);
+modalCloseBtn.addEventListener('click',closeModal);
+modal.addEventListener('click',closeModal);
